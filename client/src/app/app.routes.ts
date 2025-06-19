a311a98ac22716d151917134c0dd1fbffe554f84
RenzoQuispe <renzo123cd@gmail.com>
2025-06-19 01:47:43 -0500

    actualizar componente app principal y rutas

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { LoginComponent } from './pages/login/login';
+import { RegisterComponent } from './pages/register/register';
+import { PerfilComponent } from './pages/perfil/perfil';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: '**', redirectTo: '' }
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavbarComponent } from './components/navbar/navbar';
+import { FooterComponent } from './components/footer/footer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, FooterComponent, RouterOutlet], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'client';
-}
+export class App { }
