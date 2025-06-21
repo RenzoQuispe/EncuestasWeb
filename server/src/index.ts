036a8ff11988b261f96ee444306431e810642846
RenzoQuispe <renzo123cd@gmail.com>
2025-06-21 18:35:37 -0500

    actualizar types y dependencias

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { testDBConnection } from './database';
 import authRoutes from './routes/auth.routes'
 dotenv.config();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 //rutas
 app.use("/api/auth", authRoutes);
 
